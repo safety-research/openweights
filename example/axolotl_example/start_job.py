42ac87767b95bf5a4d35e866dc74b80253456264
Niels Warncke <niels.warncke@gmail.com>
2025-03-11 19:38:14 +0100

    fix axolotl example

--- example/axolotl_example/start_job.py
+++ example/axolotl_example/start_job.py
@@ -7,6 +7,6 @@ ow = OpenWeights()
 job = ow.axolotl.create(
     local_config_dir=os.path.dirname(__file__),
     command='axolotl train llama_3_70b_fft.yaml',
-    allowed_hardware=['8x H100']
+    allowed_hardware=['8x H100', '8x A100', '8x H100S', '8x H100N', '8x A100S']
 )
 print(job)
