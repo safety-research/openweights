1c1bdb56d4ff434532ff35fc01a13dbc6a835ade
Niels Warncke <niels.warncke@gmail.com>
2025-04-03 12:47:06 +0200

    gemma3 axolotl example works

--- example/axolotl_qwen25/start_axolotl_job.py
+++ example/axolotl_qwen25/start_axolotl_job.py
@@ -2,11 +2,11 @@ import os
 from openweights import OpenWeights
 import openweights.jobs.axolotl
 
-command = """axolotl train config.yaml"""
+
 ow = OpenWeights()
 job = ow.axolotl.create(
     local_config_dir=os.path.dirname(__file__),
-    command=command,
+    command="axolotl train qwen25.yaml",
     allowed_hardware=['8x H100', '8x H100S', '8x H100N', '8x H200']
 )
 print(job)
